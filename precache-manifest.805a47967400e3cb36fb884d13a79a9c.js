f837a8812ba2d5a9b2aad1ba610cf2be5c8baf5a
mei4296 <meiarasan@outlook.com>
2024-12-21 22:15:36 +0530

    Add files via upload

---
+++ precache-manifest.805a47967400e3cb36fb884d13a79a9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a12949aa3fcddb3cf89dbe4a70294be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7a011a18d030bce8cdb",
+    "url": "/static/css/main.a6076e18.chunk.css"
+  },
+  {
+    "revision": "64186b7ef08fd5375ae4",
+    "url": "/static/js/2.ac168658.chunk.js"
+  },
+  {
+    "revision": "d7a011a18d030bce8cdb",
+    "url": "/static/js/main.c73e9dc3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
